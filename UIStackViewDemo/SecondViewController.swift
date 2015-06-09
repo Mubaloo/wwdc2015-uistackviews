19a59dbbe40fb7094aeb739be6e8b18265d2a40c
Rich Hodgkins <rhodgkins@gmail.com>
2015-06-09 21:21:27 +0100

    Initial project commit

---
+++ UIStackViewDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  UIStackViewDemo
+//
+//  Created by Richard Hodgkins on 09/06/2015.
+//  Copyright © 2015 Rich H. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
