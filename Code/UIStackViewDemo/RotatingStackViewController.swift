138c60c19ab2420efc403e908d32a9c4d2b0bfac
Rich Hodgkins <rhodgkins@gmail.com>
2015-06-15 22:16:58 +0100

    Removed debug code

--- Code/UIStackViewDemo/RotatingStackViewController.swift
+++ Code/UIStackViewDemo/RotatingStackViewController.swift
@@ -10,41 +10,5 @@ import UIKit
 
 final class RotatingStackViewController: UIViewController {
 
-    @IBOutlet private weak var stack: UIStackView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-    
-    override func willTransitionToTraitCollection(newCollection: UITraitCollection, withTransitionCoordinator coordinator: UIViewControllerTransitionCoordinator) {
-        super.willTransitionToTraitCollection(newCollection, withTransitionCoordinator: coordinator)
-        
-        coordinator.animateAlongsideTransition({ _ in
-            
-            self.stack?.axis = newCollection.verticalSizeClass == .Compact ? .Horizontal : .Vertical
-            
-        }) { _ in
-            
-            
-//            print("Comp: V - \(self.stack?.axis == .Vertical)")
-        }
-    }
-    
-    override func traitCollectionDidChange(previousTraitCollection: UITraitCollection?) {
-        super.traitCollectionDidChange(previousTraitCollection)
-        
-        print("\(previousTraitCollection) -> \n\(traitCollection)")
-//
-//        print(stack.axis == .Vertical)
-//        view.layoutIfNeeded()
-//        
-//        stack.axis = traitCollection.verticalSizeClass == .Compact ? .Horizontal : .Vertical
-        
-        //        print(stack)
-//        print("DID: V - \(stack.axis == .Vertical)")
-//        print("DID: S - \(stack.spacing)")
-//        view.layoutIfNeeded()
-    }
 }
 
